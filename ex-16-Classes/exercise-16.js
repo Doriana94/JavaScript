cfc4d9a30cf67d3cb37d204acbedceeb1a587239
Doriana94 <131966751+Doriana94@users.noreply.github.com>
2023-06-17 18:00:16 +0200

    Update exercise-16.js

--- ex-16-Classes/exercise-16.js
+++ ex-16-Classes/exercise-16.js
@@ -4,7 +4,7 @@ class Person{
 constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-}
+    }
 }
 const developer = new Person('Mario', 'Rossi');
 console.log(developer.firstName + " " + developer.lastName);
