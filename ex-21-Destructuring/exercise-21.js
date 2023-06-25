72fd1e0165096afbc6d77c4a972a669b82b22b74
Doriana94 <131966751+Doriana94@users.noreply.github.com>
2023-06-25 17:52:08 +0200

    Update exercise-21.js
    exercise correct

--- ex-21-Destructuring/exercise-21.js
+++ ex-21-Destructuring/exercise-21.js
@@ -1,10 +1,11 @@
-const numbers = [10, 20];
 
 let num1 = 10;
 let num2 = 20;
 console.log('Before swap: ', num1, num2); // Before swap: 10 20
 
 // Variable swapping
+
+const numbers = [num2, num1];
 let temp = num2;
 num2 = num1;
 num1 = temp;
